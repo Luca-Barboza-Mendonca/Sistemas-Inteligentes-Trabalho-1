38408a52b71da1e2307488a4364fa2cf42006a44
Luca-Barboza-Mendonca <lucabarbozamendonca@gmail.com>
2024-05-08 16:39:13 -0300

    first commit

---
+++ output_analysis.py
@@ -0,0 +1,26 @@
+import matplotlib.pyplot as plt
+import json
+import numpy as np
+from scipy.stats import linregress
+
+data = []
+real = []
+pred = []
+with open("resultados.json", "r") as f:
+    data = json.load(f)
+    real = np.array([d["real"] for d in data])
+    pred = np.array([d["predicao"] for d in data])
+
+slope, intercept, _, _, _ = linregress(real, pred)
+
+plt.scatter(real, pred, label='Data Points')
+
+plt.plot(real, slope * real + intercept, color='red', label='Line of Best Fit')
+plt.plot(real, real, color='green', linestyle='--', label="Perfect Distribution")
+
+plt.xlabel("Real")
+plt.ylabel("Predicted")
+plt.title("Real Values vs Fuzzy Values")
+plt.legend()
+
+plt.show()
---
+++ terrenos1.py
@@ -0,0 +1,64 @@
+import matplotlib.pyplot as plt
+import numpy as np
+import skfuzzy as fuzz
+from skfuzzy import control as ctrl
+import json
+
+area = ctrl.Antecedent(np.arange(0,700, 1), "area")
+distancia = ctrl.Antecedent(np.arange(3600,0, -1), "distancia")
+preco = ctrl.Consequent(np.arange(0, 500000, 1), "preco")
+
+area.automf(3)
+
+distancia['good'] = fuzz.trimf(distancia.universe, [0,0, 1800])
+distancia['average'] = fuzz.trimf(distancia.universe, [0,1800, 3600])
+distancia['poor'] = fuzz.trimf(distancia.universe, [1800,3600, 3600])
+
+preco.automf(3)
+
+# area.view()
+# distancia.view()
+# preco.view()
+
+rule1 = ctrl.Rule(area['poor'] | distancia['poor'], preco['poor'])
+
+rule2 = ctrl.Rule(area['average'] | distancia['average'], preco['average'])
+
+rule3 = ctrl.Rule(area['good'] | distancia['good'], preco['good'])
+
+preco_ctrl = ctrl.ControlSystem([rule1, rule2, rule3])
+precificador = ctrl.ControlSystemSimulation(preco_ctrl)
+
+# precificador.input['area'] = 420
+# precificador.input['distancia'] = 260
+
+terrenos = []
+
+with open('terrenos.json', 'r') as f:
+    terrenos = json.load(f)['terrenos']
+
+val = []
+
+for t in terrenos:
+    input_area = t['area']
+    input_distancia = t['distancia']
+
+    precificador.input['area'] = input_area
+    precificador.input['distancia'] = input_distancia
+
+    precificador.compute()
+
+    out = precificador.output['preco']
+    real = t["preco"]
+    loss = (abs(real - out)/real) * 100
+    val.append({"nome": t["nome"],"predicao": out, "real": real, "Erro percentual": loss})
+    print(out)
+
+with open("resultados.json", "w") as f:
+    json.dump(val, f)
+# preco.view(sim=precificador)
+
+print(val)
+
+plt.show()
+
